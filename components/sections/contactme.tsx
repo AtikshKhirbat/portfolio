2313b2e8c6866762707731f6c6badeb607be6992
Atiksh Khirbat <76040754+AtikshKhirbat@users.noreply.github.com>
2025-12-20 20:15:15 +0530

    Update contactme.tsx

--- components/sections/contactme.tsx
+++ components/sections/contactme.tsx
@@ -38,9 +38,9 @@ function MailSection() {
       <div className="gap-2.5 w-full h-fit flex flex-col">
         <p className="text-5xl">Let&apos; Connect!!!</p>
         <p className="font-vt text-2xl">
-          Drop me an email at xyz@gmail.com or say hi on my social media!!! I am
-          always open to discussing your creative ideas and bring your visions
-          to life!!!
+          Drop me an email at atikshkhirbat0@gmail.com or say hi on my social
+          media!!! I am always open to discussing your creative ideas and bring
+          your visions to life!!!
         </p>
       </div>
       <div className="gap-3.5 flex flex-col w-full h-fit">
@@ -48,14 +48,17 @@ function MailSection() {
           <div className="border-2 border-border bg-primary p-2.5">
             <GrMail className="size-6 text-text" />
           </div>
-          <p className="text-2xl">xyz@gmail.com</p>
+          <p className="text-2xl">atikshkhirbat0@gmail.com</p>
         </div>
         <div className="w-full h-fit flex flex-col md:flex-row py-2.5 gap-3.5">
-          <SocialButton href="">
+          <SocialButton href="https://github.com/AtikshKhirbat" target="_blank">
             <FaGithub className="size-7" />
             <p className="text-2xl">Github</p>
           </SocialButton>
-          <SocialButton href="">
+          <SocialButton
+            href="https://www.linkedin.com/in/atikshkhirbat/"
+            target="_blank"
+          >
             <FaLinkedin className="size-7" />
             <p className="text-2xl">Linkedin</p>
           </SocialButton>
