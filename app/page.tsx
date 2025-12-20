514b417ceacde2fc47e35b6e52a051582b056181
Atiksh Khirbat <76040754+AtikshKhirbat@users.noreply.github.com>
2025-12-20 20:15:42 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,12 +9,10 @@ export default function Home() {
     <div className="flex flex-col md:gap-y-48 min-h-screen bg-background font-jersey">
       <NavBar />
 
-      {/* Hero Section - centered on screen */}
       <div className="flex-1 flex items-center justify-center xl:px-20 min-h-screen mt-10 md:mt-32 xl:mt-0">
         <HeroSection />
       </div>
 
-      {/* About Section */}
       <div className="xl:px-20 h-fit justify-center flex flex-1 items-center overflow-clip">
         <AboutMe />
       </div>
