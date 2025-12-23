b0de7a432a495c6bb26f69e97733ba844d38e7d6
Atiksh Khirbat <76040754+AtikshKhirbat@users.noreply.github.com>
2025-12-23 12:05:53 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ const vtFont = VT323({
 export const metadata: Metadata = {
   title: "Atiksh Khirbat",
   description:
-    "Explore Atiksh's Portfolio, showcasing projects and his expertise in full-stack development in working with frameworks like React, Next.js, Node.js, tailwind CSS and more. Tune in to see a new insight into design and its implementation",
+    "Explore Atiksh Khirbat's Portfolio, showcasing projects and his expertise in full-stack development in working with frameworks like React, Next.js, Node.js, tailwind CSS and more. Tune in to see a new insight into design and its implementation",
 };
 
 export default function RootLayout({
